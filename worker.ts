c829de6a5550e7e0f2d5a299739335ff05a1921d
dreamzzzz120-cell <dreamzzzz120@gmail.com>
2026-08-17 22:11:56 -0700

    security: run hardened webhook delivery worker

--- worker.ts
+++ worker.ts
@@ -1,11 +1,16 @@
 import { runWorkerLoop } from './src/workers/osv-worker.ts';
 import { runMonitoringWorkerLoop } from './src/workers/monitoring-worker.ts';
+import { runWebhookWorkerLoop } from './src/workers/webhook-worker.ts';
 
 async function main() {
   try {
-    await Promise.all([runWorkerLoop(), runMonitoringWorkerLoop()]);
+    await Promise.all([
+      runWorkerLoop(),
+      runMonitoringWorkerLoop(),
+      runWebhookWorkerLoop(),
+    ]);
   } catch (error) {
-    console.error('[Worker] Fatal error:', error);
+    console.error('[Worker] Fatal error:', error instanceof Error ? error.message : error);
     process.exit(1);
   }
 }
