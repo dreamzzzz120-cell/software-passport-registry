e6eea4f0cad58707383d1a916d4ba0f657bc8df3
dreamzzzz120-cell <dreamzzzz120@gmail.com>
2026-08-18 00:36:50 -0700

    fix: augment Express core Request with sprApi

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,16 +1,25 @@
 import 'express';
+import 'express-serve-static-core';
+
+type SprApiContext = {
+  id: string;
+  tenantId: string;
+  name: string;
+  scopes: readonly string[];
+};
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    sprApi?: SprApiContext;
+  }
+}
 
 declare global {
   namespace Express {
     interface Request {
-      sprApi?: {
-        id: string;
-        tenantId: string;
-        name: string;
-        scopes: readonly string[];
-      };
+      sprApi?: SprApiContext;
     }
   }
 }
 
-export {};
+export {}; 
