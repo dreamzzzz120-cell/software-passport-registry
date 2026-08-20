72eb9b543cb9864cf009ab6f03b4061cbaac1d4f
dreamzzzz120-cell <dreamzzzz120@gmail.com>
2026-08-19 22:25:11 -0700

    security: enforce frontend Firebase session boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import AuthGate from './components/AuthGate';
 import './index.css';
 import './login-premium.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthGate>
+      <App />
+    </AuthGate>
   </StrictMode>,
 );
