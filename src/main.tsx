3317c04ee3d66ecae1224f707f38d1ac6760c4eb
dreamzzzz120-cell <dreamzzzz120@gmail.com>
2026-08-22 14:59:12 -0700

    perf: lazy-load application shell from entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import LazyApp from './LazyApp';
 import './index.css';
 import './styles/spr-shell.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <LazyApp />
   </React.StrictMode>,
 );
