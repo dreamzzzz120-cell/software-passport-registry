01e183325938a5f4e3c357c9b779e2ec8b5d3306
dreamzzzz120-cell <dreamzzzz120@gmail.com>
2026-09-04 00:16:06 -0700

    test: cover hex IPv4-mapped IPv6 SSRF cases

--- src/integrations/hardening.test.ts
+++ src/integrations/hardening.test.ts
@@ -38,6 +38,7 @@ describe('outbound SSRF protection', () => {
     '127.0.0.1', '10.0.0.1', '172.16.0.1', '192.168.1.1', '169.254.169.254',
     '100.64.0.1', '198.18.0.1', '::1', 'fc00::1', 'fd12:3456::1', 'fe80::1',
     '::ffff:127.0.0.1', '::ffff:169.254.169.254',
+    '::ffff:7f00:1', '::ffff:a9fe:a9fe',
   ])('blocks non-public address %s', (address) => {
     expect(blockPrivateAddress(address)).toBe(true);
   });
